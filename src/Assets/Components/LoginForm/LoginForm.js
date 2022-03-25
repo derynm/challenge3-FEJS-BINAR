f8c0a9476d4477ba4fc63b1114c516c01d80682d
Dery <69612416+derynm@users.noreply.github.com>
2022-03-25 21:43:45 +0700

    fix login page

--- src/Assets/Components/LoginForm/LoginForm.js
+++ src/Assets/Components/LoginForm/LoginForm.js
@@ -1,29 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import "./comp-login.css";
 import { AlertLogin } from "../Alerts/AlertLogin";
-import { Link } from "react-router-dom";
+import logo from "../../Img/logo.png";
+import { useNavigate } from "react-router-dom";
 
 export const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [alertStats, setAlertStats] = useState(false);
+  let Navigate = useNavigate();
+
+  let handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  let handlePass = (e) => {
+    setPass(e.target.value);
+  };
+
+  let validation = () => {
+    if (email == "dery@mail" && pass == "123") {
+      Navigate("dashboard", { replace: true });
+    } else {
+      setAlertStats(true);
+    }
+  };
+
   return (
     <div>
       <Form>
+        <img src={logo} alt="logo" />
         <h3>Welcome, Admin BCR</h3>
-        <AlertLogin />
+        {alertStats ? <AlertLogin /> : null}
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Contoh: johndee@gmail.com" />
+          <Form.Control
+            type="email"
+            placeholder="Contoh: johndee@gmail.com"
+            onChange={handleEmail}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="6+ karakter" />
+          <Form.Control
+            type="password"
+            placeholder="6+ karakter"
+            onChange={handlePass}
+          />
         </Form.Group>
-        <Link to={"dashboard"}>
-          <Button variant="primary" type="submit" id="btn-login">
-            Submit
-          </Button>
-        </Link>
+
+        <Button
+          variant="primary"
+          type="submit"
+          id="btn-login"
+          onClick={validation}
+        >
+          Submit
+        </Button>
       </Form>
     </div>
   );
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Navbar } from '../../Assets/Components/Navbar/Navbar'
 import { SideBar } from '../../Assets/Components/SideBar/SideBar'
 
 export const Dashboard = () => {
